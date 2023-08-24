e6d970bb5bbb6bc74c6f9d9e27c6035e9d68a2be
Luna Maria de Lima  Moura <rm86086@estudante.fieb.edu.br>
2023-08-24 08:58:07 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/tcc/inf2cm/DLAL_tcc/DlalTccApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.inf2cm.DLAL_tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlalTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlalTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/inf2cm/DLAL_tcc/DlalTccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.inf2cm.DLAL_tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlalTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
