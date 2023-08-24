433affd7df35140ec02a64aa9e982101a357c163
Luna Maria de Lima  Moura <rm86086@estudante.fieb.edu.br>
2023-08-24 09:54:03 -0300

    criação do model cliente

---
+++ src/main/java/com/tcc/inf2cm/DLAL_tcc/model/Cliente.java
@@ -0,0 +1,66 @@
+package com.tcc.inf2cm.DLAL_tcc.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="clientes")
+public class Cliente {
+	
+	@Id // Definição de chave primária PK
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Auto-Incremento
+	
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	private String nome;
+	private String cpf;
+	private String cep;
+	private String bairro;
+	private String cidade;
+	private String telefone;
+
+}
